b1aa8424b3ed45fa76202d48d914cae9f44971d9
anautovic <afk1995wilfried@gmail.com>
2022-12-13 17:07:11 +0100

    first commit

--- my-app/src/data/MenuData.js
+++
@@ -1,6 +0,0 @@
-export const menuData = [
-    {title: 'Accueil', link:'/' },
-    {title: 'E-shop', link:'/maboutique'},
-    {title: 'Formation', link:'/formation'},
-    {title: 'Gallerie', link:'/gallerie'}
-]
