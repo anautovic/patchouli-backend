9ccce52e1f5c7cac5be3ec25ab2fceb1a8f81055
anautovic <afk1995wilfried@gmail.com>
2022-12-13 16:21:09 +0100

    sitemap commit

--- backend/server.js
+++ backend/server.js
@@ -42,10 +42,10 @@ const __dirname = path.resolve()
 app.use('/uploads', cors(corsOptions), express.static(path.join(__dirname, '/uploads')))
 
 if (process.env.NODE_ENV === 'production') {
-  app.use( cors(corsOptions) ,express.static(path.join(__dirname, '/my-app/build')))
+  app.use( cors(corsOptions) ,express.static(path.join(__dirname, '../my-app/build')))
 
   app.get('*', cors(corsOptions), (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'my-app', 'build', 'index.html'))
+    res.sendFile(path.resolve(__dirname, '../my-app', 'build', 'index.html'))
   )
 } else {
   app.get('/', (req, res) => {
